dd4a0239484108c2a1c0ad289628cd6ac6fd334c
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-09-30 01:42:36 +0700

    ref: sort return err

--- db/squirrel/sort/sort.go
+++ db/squirrel/sort/sort.go
@@ -1,17 +1,24 @@
 package sort
 
 import (
+	"errors"
 	"strings"
 
 	sq "github.com/Masterminds/squirrel"
 )
+
+var errEmptyColumn = errors.New("column cannot be empty string")
 
 type Sort struct {
 	column string
 	order  string
 }
 
-func New(column, order string) Sort {
+func New(column, order string) (Sort, error) {
+	if column == "" {
+		return Sort{}, errEmptyColumn
+	}
+
 	order = strings.ToUpper(order)
 	if order != "ASC" && order != "DESC" {
 		order = "ASC"
@@ -20,7 +27,7 @@ func New(column, order string) Sort {
 	return Sort{
 		column: column,
 		order:  order,
-	}
+	}, nil
 }
 
 // UseSelectBuilder adds sort to squirrel.SelectBuilder
