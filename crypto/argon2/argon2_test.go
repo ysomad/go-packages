15ec9a58d15b6e0b6a79d096a02816fd526efc41
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-10-05 01:12:17 +0700

    separated argon2 to its own pkg

--- crypto/argon2/argon2_test.go
+++ crypto/argon2/argon2_test.go
@@ -2,40 +2,40 @@ package argon2
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestCompare(t *testing.T) {
 	tests := []struct {
-		name    string
-		plain   string
-		hash    string
-		ok      bool
-		wantErr bool
+		name  string
+		plain string
+		hash  string
+		ok    bool
 	}{
 		{
-			name:    "correct match",
-			plain:   "inanzzz",
-			hash:    "$argon2id$v=19$m=65536,t=1,p=4$3Ymrg20KqXd9mMawXP/YzA$nf7ubeO0tB1NDk4nBscgsHvIcDECMjIuEeEjgBRMe3s",
-			ok:      true,
-			wantErr: false,
+			name:  "correct match",
+			plain: "inanzzz",
+			hash:  "$argon2id$v=19$m=65536,t=1,p=4$3Ymrg20KqXd9mMawXP/YzA$nf7ubeO0tB1NDk4nBscgsHvIcDECMjIuEeEjgBRMe3s",
+			ok:    true,
 		},
 		{
-			name:    "incorrect match",
-			plain:   "test123",
-			hash:    "$argon2id$v=19$m=65536,t=1,p=4$L/6i/DD9Ie5dKo7L6PpvVg$8OLc5G2E715nbsSgA4ZKcGngVLtAeCnB4CD76XbShic",
-			ok:      false,
-			wantErr: false,
+			name:  "incorrect match",
+			plain: "test123",
+			hash:  "$argon2id$v=19$m=65536,t=1,p=4$L/6i/DD9Ie5dKo7L6PpvVg$8OLc5G2E715nbsSgA4ZKcGngVLtAeCnB4CD76XbShic",
+			ok:    false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			a := New()
-			got, err := a.Compare(tt.plain, tt.hash)
+			argon2 := New()
+			got, err := argon2.Compare(tt.plain, tt.hash)
 
-			assert.Equal(t, tt.wantErr, (err != nil))
-			assert.Equal(t, tt.ok, got)
+			if err != nil {
+				t.Errorf("want no error, got %s", err.Error())
+			}
+
+			if tt.ok != got {
+				t.Errorf("want %t, got %t", tt.ok, got)
+			}
 		})
 	}
 }
